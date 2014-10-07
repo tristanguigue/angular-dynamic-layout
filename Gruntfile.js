159142bfcd6f2d54e88e23db1745640e4dc95f9c
Tristan Guigue <tristan@tyba.com>
2014-10-07 22:24:44 +0200

    Added Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+// Gruntfile.js
+
+// our wrapper function (required by grunt and its plugins)
+// all configuration goes inside this function
+module.exports = function(grunt) {
+
+  // ===========================================================================
+  // CONFIGURE GRUNT ===========================================================
+  // ===========================================================================
+  grunt.initConfig({
+
+    // get the configuration info from package.json ----------------------------
+    // this way we can use things like name and version (pkg.name)
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      // when this task is run, lint the Gruntfile and all js files in src
+        build: ['Grunfile.js', 'src/**/*.js']
+      },
+    uglify: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          'dist/isogrid.min.js':  ['src/isogrid.js', 
+                                   'src/services/FilterService.js',
+                                   'src/services/OrderService.js',
+                                   'src/services/PositionService.js'] 
+        }
+      }
+    }
+  });
+
+  // ===========================================================================
+  // LOAD GRUNT PLUGINS ========================================================
+  // ===========================================================================
+  // we can only load these if they are in our package.json
+  // make sure you have run npm install so our app can find these
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['jshint', 'uglify']);
+};
+
+
+
