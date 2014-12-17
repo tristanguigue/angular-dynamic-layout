1791df0e25ab57ecde836b88b1538b459e6f5842
Tristan Guigue <tristan@tyba.com>
2014-12-17 16:09:54 +0100

    Update IsoGrid version

--- bower_components/isogrid/src/services/FilterService.js
+++ bower_components/isogrid/src/services/FilterService.js
@@ -1,73 +1,134 @@
+/**
+* The filter service
+*
+* Allows filters in Conjuctive Normal Form using the item's property or any
+* custom operation on the items
+*
+* For example: 
+*        var filters = [ // an AND goup compose of OR groups
+*          [ // an OR group compose of statements
+*            ['color', '=', 'grey'], // A statement
+*            ['color', '=', 'black']
+*          ],
+*          [ // a second OR goup composed of statements
+*            ['atomicNumber', '<', 3]
+*          ]
+*        ];
+* Or
+*        var myCustomFilter = function(item){
+*          if(item.color != 'red')
+*            return true;
+*          else
+*            return false;
+*        };
+*
+*        filters = [
+*          [myCustomFilter]
+*        ];
+*
+*/
+
 isoGridModule.factory('FilterService', function () {
-      var COMPARATORS = ['=', '<', '>', '<=', '>=', '!=', 'in', 'not in', 'contains'];
+  // The list of available comparators
+  var COMPARATORS = ['=', '<', '>', '<=', '>=', '!=', 
+                     'in', 'not in', 'contains'];
 
-      var checkStatement = function(item, statement){
-                if(typeof(statement) == "function")
-                  return statement(item);                    
-                
-                else{
-                  if(statement.length<2)
-                    throw "Incorrect statement";
-                  
-                  var property = statement[0];
-                  var comparator = statement[1];
-                  var value = statement[2];
-                  
-                  if(COMPARATORS.indexOf(comparator) === -1)
-                    throw "Incorrect statement comparator: " +  comparator;
+  /**
+  * Check if a single item passes the single statement criteria
+  * @param item: the item being probed
+  * @param statement: the criteria being use to test the item
+  * @return true if the item passed the statement, false otherwise
+  */
+  var checkStatement = function(item, statement){
+    // If the statement is a custom filter, we give the item as a parameter
+    if(typeof(statement) == "function")
+      return statement(item);                    
 
-                  if(!item[property])
-                    return false;
+    // If the statement is a regular filter, it has to be with the form
+    // [propertyName, comparator, value]    
+    else{
+      if(statement.length<2)
+        throw "Incorrect statement";
+      
+      var property = statement[0];
+      var comparator = statement[1];
+      var value = statement[2];
+      
+      if(COMPARATORS.indexOf(comparator) === -1)
+        throw "Incorrect statement comparator: " +  comparator;
 
-                  switch(comparator){
-                    case '=':
-                      return item[property] == value;
-                    case '<':
-                      return item[property] < value;
-                    case '<=':
-                      return item[property] <= value;
-                    case '>':
-                      return item[property] > value;
-                    case '>=':
-                      return item[property] >= value;
-                    case '!=':
-                      return item[property] != value;
-                    case 'in':
-                      return item[property] in value;
-                    case 'not in':
-                      return !(item[property] in value);
-                    case 'contains':
-                      if(!(item[property] instanceof Array))
-                        throw "contains statement has to be applied on array";
-                      return item[property].indexOf(value) > -1;
-                  }
-                }        
-      };
+      if(!item[property])
+        return false;
 
-      var checkOrGroup = function(item, orGroup){
-              for(var j in orGroup){
-                if(checkStatement(item, orGroup[j]))
-                  return true;
-              }
-              return false;
-      };
+      switch(comparator){
+        case '=':
+          return item[property] == value;
+        case '<':
+          return item[property] < value;
+        case '<=':
+          return item[property] <= value;
+        case '>':
+          return item[property] > value;
+        case '>=':
+          return item[property] >= value;
+        case '!=':
+          return item[property] != value;
+        case 'in':
+          return item[property] in value;
+        case 'not in':
+          return !(item[property] in value);
+        case 'contains':
+          if(!(item[property] instanceof Array))
+            throw "contains statement has to be applied on array";
+          return item[property].indexOf(value) > -1;
+      }
+    }
+  };
 
-      var checkAndGroup = function(item, andGroup){
-            for(var i in andGroup){
-              if(!checkOrGroup(item, andGroup[i]))
-                return false;
-            }
-            return true;
-      };
+  /**
+  * Check a sub (or) group
+  * @param item: the item being probed
+  * @param orGroup: the array of statement use to probe the item
+  * @return true if the item passed at least one of the statements, 
+  * false otherwise
+  */
+  var checkOrGroup = function(item, orGroup){
+    for(var j in orGroup){
+      if(checkStatement(item, orGroup[j]))
+        return true;
+    }
+    return false;
+  };
 
-      return {
-        apply: function (items, filters) {
-          var retItems = [];
-          for(var i in items){
-            if(checkAndGroup(items[i], filters))
-              retItems.push(items[i]);
-          }
-          return retItems;
-        }
-      };
+  /**
+  * Check the main group
+  * @param item: the item being probed
+  * @param orGroup: the array of or groups use to probe the item
+  * @return true if the item passed all of of the or groups, 
+  * false otherwise
+  */
+  var checkAndGroup = function(item, andGroup){
+    for(var i in andGroup){
+      if(!checkOrGroup(item, andGroup[i]))
+        return false;
+    }
+    return true;
+  };
+
+  return {
+    /**
+    * Check which of the items passes the filters
+    * @param items: the items being probed
+    * @param filters: the array of and groups use to probe the item
+    * @return the list of items that passes the filters
+    */
+    apply: function (items, filters) {
+      var retItems = [];
+      for(var i in items){
+        if(checkAndGroup(items[i], filters))
+          retItems.push(items[i]);
+      }
+      return retItems;
+    }
+  };
 });
