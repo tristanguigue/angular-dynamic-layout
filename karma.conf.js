36860e830f281eed2e80317c6bf730ef31f2cb77
Tristan Guigue <tristan@tyba.com>
2015-01-02 20:57:37 +0100

    Reverted list of browsers for karma

--- karma.conf.js
+++ karma.conf.js
@@ -62,7 +62,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+    browsers: ['Firefox', 'Chrome', 'Safari'],
 
 
     // Continuous Integration mode
