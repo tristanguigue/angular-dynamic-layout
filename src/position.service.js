70134d3db1b42860b1d8763bb9a340813ed51639
csnyders <craig.snyders@os.uk>
2015-12-04 00:37:36 +0000

    Fix bug where item columnspan could sometimes be incorrectly calculated

--- src/position.service.js
+++ src/position.service.js
@@ -316,7 +316,7 @@
     function setItemsColumnSpan(colSize) {
       var i;
       for (i = 0; i < items.length; ++i) {
-        items[i].columnSpan = Math.ceil(items[i].width / colSize);
+        items[i].columnSpan = Math.round(items[i].width / colSize);
       }
     }
 
