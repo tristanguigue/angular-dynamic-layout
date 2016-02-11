2161a58af7a98ce9aac4de5200a1edb67620d519
Falk Romano <fr@world-architects.com>
2016-02-11 08:55:22 +0100

    simplified the code which fixes #24
    Thanks to @aschmoe and his comment: https://github.com/tristanguigue/angular-dynamic-layout/pull/35#discussion_r52546460

--- src/position.service.js
+++ src/position.service.js
@@ -289,17 +289,8 @@
      * Then get biggest item position and set it as dynamic-layout height
      */
     function setDynamicLayoutHeight(columns) {
-      var i;
-      var j;
-      var dlHeight = [];
-
-      for (i = 0; i < columns.length; ++i) {
-        for (j = 0; j < columns[i].length; ++j) {
-          dlHeight[i] = columns[i][columns[i].length - 1].y + columns[i][columns[i].length - 1].height;
-        }
-      }
-
-      angular.element('[dynamic-layout]').css('height', Math.max.apply(null, dlHeight));
+      var columnsHeights = getColumnsHeights(columns);
+      angular.element('[dynamic-layout]').css('height', Math.max.apply(null, columnsHeights));
     }
 
     /*
